810830d030a9e29862568f3be3a919c0d43b46a2
NetroTypeShi <andresmenduina@gmail.com>
2025-10-06 10:59:36 +0200

    sasaasd

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -7,17 +7,24 @@ public class FirstPersonCamera : MonoBehaviour
     [SerializeField] float sensibility;
     float mouseX;
     float mouseY;
+    float xRotation;
     public Transform playerTransform;
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         mouseX = Input.GetAxis("Mouse X") * sensibility * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse X") * sensibility * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, -90f); 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
 
     }
 }
